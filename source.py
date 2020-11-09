3848ba69224ed9385c342909f93b2e54686ffd2e
Alejandro Rodriguez <alex.rdz.chavi@gmail.com>
2020-11-09 07:08:07 -0600

    Se agrego el menu

---
+++ source.py
@@ -0,0 +1,30 @@
+contador = 1
+
+while contador ==1:
+    
+    eleccion = int(input("Que accion deseas realizar?\n 1.Registrar una venta\n  2.Consultar ventas de un dia en especifico\n  3.Salir\n"))
+    
+    if eleccion == 1:
+        pass
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    if eleccion == 2:
+        pass
+    
+    
+    
+    
+    
+    
+    
+    if eleccion == 3:
+        
+        contador = 2
