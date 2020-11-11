b36a11437aa3453be292c822cd9b98dd74b54143
Alejandro Rodriguez <alex.rdz.chavi@gmail.com>
2020-11-11 06:08:25 -0600

    Se agregaron variables en registro de ventas y el for para meter valores en las listas

--- source.py
+++ source.py
@@ -13,7 +13,18 @@ while contador ==1:
     eleccion = int(input("Que accion deseas realizar?\n 1.Registrar una venta\n 2.Consultar ventas de un dia en especifico\n 3.Salir\n"))
     
     if eleccion == 1:
-        pass
+        cant = int(input("Cuantas ventas quieres registrar?"))
+        for i in range(cant):
+            nombre_prd = input("Nombre de producto\n")
+            lista_nombre.append(nombre_prd)
+            cantidad_prd = int(input("Dime la cantidad de productos vendidos\n"))
+            lista_cantidad.append(cantidad_prd)
+            precio_prd = float(input("Dime el precio del producto\n"))
+            lista_precios.append(precio_prd)
+            fecha_v = datetime.date.today()
+            lista_fecha.append(fecha_v)
+            precios_totales.append((cantidad_prd) * (precio_prd))
+            total_venta = sum(precios_totales)
     
     
     
